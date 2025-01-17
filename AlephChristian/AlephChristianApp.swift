afc9dda4fff61eb5fe42cdb36d1688ec95b8e82b
Christian Gunawan <cg404cgunawan@gmail.com>
2025-01-17 20:58:45 +0700

    Initial Commit

---
+++ AlephChristian/AlephChristianApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlephChristianApp.swift
+//  AlephChristian
+//
+//  Created by Christian Gunawan on 17/01/25.
+//
+
+import SwiftUI
+
+@main
+struct AlephChristianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
